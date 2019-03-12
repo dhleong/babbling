ef11c159685e7bf98b678e9e065375771d247d09
Daniel Leong <me@dhleong.net>
2019-03-11 20:15:03 -0400

    Clean up index.js

--- index.js
+++ index.js
@@ -3,12 +3,3 @@ const { ChromecastDevice } = require('./lib/device');
 module.exports = {
     ChromecastDevice,
 };
-
-(async () => {
-    const device = new ChromecastDevice('Family Room TV');
-    const app = await device.openApp('youtube');
-    await app.play('GWb7zhBBnUE');
-    device.close();
-
-    console.log('done!');
-})().catch(e => console.error(e));
