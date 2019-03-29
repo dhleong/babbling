47656995b3a3dc090440ce38b9469ab53456958b
Daniel Leong <falcone88@gmail.com>
2019-03-28 23:18:39 -0400

    Add HBO Go Support (#2)
    * Rough, initial support for HBO Go
    
    There's still quite a bit of uncertainty, but it's functional!
    
    * hbo: Add startTime option to play()
    
    * Attempt to support resumeSeries method with HBO Go
    
    I'm not convinced the receiver app actually updates the marker
    automatically, but... maybe. Incorporating that into this library would
    be tricky for CLI use (since, unlike the other services, we'd have to
    keep the app running after the show starts in order to keep that
    updated) but in a long-running application it might be reasonable....
    
    Will test later to see if it's even a problem
    
    * Refactor API implementation so we can remove the docs
    
    Now it's self-documenting, as it should be
    
    * Add some logging

---
+++ src/apps/hbogo/api.ts
@@ -0,0 +1,258 @@
+import jwt from "jsonwebtoken";
+import request, {OptionsWithUrl} from "request-promise-native";
+
+import _debug from "debug";
+const debug = _debug("babbling:hbogo:api");
+
+const CONTENT_URL = "https://comet.api.hbo.com/content";
+const TOKENS_URL = "https://comet.api.hbo.com/tokens";
+
+export const HBO_HEADERS = {
+    "Accept": "application/vnd.hbo.v9.full+json",
+    // tslint:disable-next-line
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/73.0.3683.86 Safari/537.36",
+    "X-Hbo-Client-Version": "Hadron/21.0.1.176 desktop (DESKTOP)",
+};
+
+export interface IMarker {
+    /** Formatted as eg: "2019-03-27T23:16:25Z" */
+    created: string;
+
+    /** The part after `urn:hbo:episode:` */
+    cutId: string;
+
+    /** The URN */
+    id: string;
+
+    /** In seconds */
+    position: number;
+
+    /** In seconds */
+    runtime: number;
+}
+
+export interface ISeriesMarker {
+    focusEpisode: string;
+    markerStatus: "START" | "LATEST" | "CONTINUE" | "TOPICAL";
+}
+
+function extractIdFromUrn(urnOrId: string) {
+    const lastColon = urnOrId.lastIndexOf(":");
+    return lastColon === -1
+        ? urnOrId
+        : urnOrId.substring(lastColon + 1);
+}
+
+export class HboGoApi {
+
+    private refreshToken: string | undefined;
+    private refreshTokenExpires = 0;
+
+    constructor(
+        private token: string,
+    ) {}
+
+    public async stopConcurrentStreams() {
+        return this.request("delete", {
+            url: "https://comet.api.hbo.com/concurrentStreams",
+        });
+    }
+
+    /**
+     * Given a series Urn, attempt to determine the Urn and
+     * start position (in seconds) of the next episode (or
+     * episode to resume) for that series
+     */
+    public async fetchNextEpisodeForSeries(seriesUrn: string) {
+        const markersBySeries = await this.getSeriesMarkers();
+        const seriesMarker = markersBySeries[seriesUrn];
+        if (!seriesMarker) {
+            // TODO we could fetch the series' episodes and pick the first
+            throw new Error("No marker for series");
+        }
+
+        // NOTE: I'm not sure what to do with `markerStatus`...
+        const focusEpisode: string = seriesMarker.focusEpisode;
+        const episodeMarker = await this.getMarkerForEpisode(focusEpisode);
+
+        const result = {
+            position: undefined as number | undefined,
+            urn: focusEpisode,
+        };
+
+        if (episodeMarker) {
+            result.position = episodeMarker.position;
+        }
+
+        return result;
+    }
+
+    /**
+     * Fetch a map of series URN to its `ISeriesMarker`. It's
+     * unclear what criteria is used to determine which series
+     * are included in this map.
+     */
+    public async getSeriesMarkers(): Promise<{[urn: string]: ISeriesMarker}> {
+        const markersResult = await this.fetchContent(["urn:hbo:series-markers:mine"]);
+        return markersResult[0].body.seriesMarkers;
+    }
+
+    /**
+     * Given `urn:hbo:episode:<ID>` or just `<ID>`,
+     * fetch a marker for the episode, or null if none
+     */
+    public async getMarkerForEpisode(
+        episodeUrnOrId: string,
+    ): Promise<null | IMarker> {
+        const result = await this.getMarkersForEpisodes([episodeUrnOrId]);
+        if (result.length) return result[0];
+        return null;
+    }
+
+    /**
+     * Given an array of `urn:hbo:episode:<ID>` or just `<ID>`,
+     * fetch a marker for each episode. Episodes for which there
+     * are no markers will not have a corresponding element in the
+     * responding array, so you should not expect the Markers in
+     * the resulting array to be in the same order as the provided
+     * array
+     */
+    public async getMarkersForEpisodes(
+        episodeUrnOrIds: string[],
+    ): Promise<IMarker[]> {
+        const ids = episodeUrnOrIds.map(extractIdFromUrn);
+
+        let episodeMarkers;
+        try {
+            episodeMarkers = await this.request("get", {
+                json: true,
+                qs: {
+                    limit: ids.length,
+                },
+                url: `https://markers.api.hbo.com/markers/${ids.join(",")}`,
+            });
+
+            debug("Loaded markers for", ids, ":", episodeMarkers);
+        } catch (e) {
+            // no marker, probably
+            debug("Error fetching marker for", ids, ":", e);
+        }
+
+        if (Array.isArray(episodeMarkers)) {
+            return episodeMarkers;
+        } else if (episodeMarkers) {
+            return [episodeMarkers];
+        }
+
+        return [];
+    }
+
+    /*
+     * Util methods
+     */
+
+    public extractTokenInfo() {
+        const tokenData = jwt.decode(this.token) as any;
+        if (!tokenData || !tokenData.payload || !tokenData.payload.tokenPropertyData) {
+            debug("Invalid token:", tokenData);
+            debug("From:", this.token);
+            throw new Error("Invalid token");
+        }
+
+        const data = tokenData.payload.tokenPropertyData;
+        const { clientId, userTkey } = data;
+        const deviceId = data.deviceSerialNumber;
+
+        return {
+            clientId,
+            deviceId,
+            userTkey,
+        };
+    }
+
+    public async getRefreshToken() {
+        if (Date.now() < this.refreshTokenExpires) {
+            debug("Reusing cached refreshToken");
+            return this.refreshToken;
+        }
+
+        debug("Loading refresh refreshToken...");
+        return this.loadRefreshToken();
+    }
+
+    private async fetchContent(urns: string[]) {
+        return this.request("post", {
+            body: urns.map(urn => ({id: urn})),
+            json: true,
+            url: CONTENT_URL,
+        });
+    }
+
+    private async request(method: "delete" | "get" | "post", opts: OptionsWithUrl) {
+        return request[method](await this.fillRequest(opts));
+    }
+
+    private async fillRequest(opts: OptionsWithUrl) {
+        const token = await this.getRefreshToken();
+        return Object.assign({
+            headers: Object.assign(HBO_HEADERS, {
+                Authorization: `Bearer ${token}`,
+            }),
+        }, opts);
+    }
+
+    private async loadRefreshToken() {
+        // NOTE: I'm not sure if this step is 100% necessary, but
+        // it may help to ensure refreshed tokens....
+
+        const {
+            clientId,
+            deviceId,
+        } = this.extractTokenInfo();
+
+        // this step fetches some sort of session token that is *not*
+        // logged in
+        const baseTokens = await request.post({
+            body: {
+                client_id: clientId,
+                client_secret: clientId,
+                deviceSerialNumber: deviceId,
+                grant_type: "client_credentials",
+                scope: "browse video_playback_free",
+            },
+            headers: HBO_HEADERS,
+            json: true,
+            url: TOKENS_URL,
+        });
+        debug("baseTokens=", baseTokens);
+
+        // now we exchange the session token above for an updated
+        // refresh token
+        const realTokens = await request.post({
+            body: {
+                grant_type: "refresh_token",
+                refresh_token: this.token,
+                scope: "browse video_playback device",
+            },
+            headers: Object.assign(HBO_HEADERS, {
+                Authorization: `Bearer ${baseTokens.refresh_token}`,
+            }),
+            json: true,
+            url: TOKENS_URL,
+        });
+        debug("Real Tokens:", realTokens);
+
+        // make sure it worked
+        if (!realTokens.isUserLoggedIn) {
+            throw new Error("Not logged in...");
+        }
+
+        // cache this until expired (see: expires_in)
+        // TODO store this?
+        this.refreshTokenExpires = Date.now() + realTokens.expires_in;
+        this.refreshToken = realTokens.refresh_token;
+
+        return realTokens.refresh_token;
+    }
+
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { ChromecastDevice } from "./device";
+export { HboGoApp, IHboGoOpts, IHboGoPlayOptions } from "./apps/hbogo";
 export { HuluApp, IHuluOpts } from "./apps/hulu";
 export { YoutubeApp, IYoutubeOpts } from "./apps/youtube";
