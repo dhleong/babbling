c044d64856681b7a362282be571e6a17b70f7164
Daniel Leong <me@dhleong.net>
2019-10-08 19:56:09 -0400

    Support generating a link code for Prime video
    Still missing a step somewhere, because Register now throws an empty
    object (`{}`) error. Probably something to do with the weird
    AmIRegistered handshake?

--- src/apps/prime/index.ts
+++ src/apps/prime/index.ts
@@ -1,6 +1,8 @@
 import _debug from "debug";
 const debug = _debug("babbling:PrimeApp");
 
+import request from "request-promise-native";
+
 import { generateDeviceId } from "chakram-ts/dist/util";
 import os from "os";
 
@@ -11,12 +13,15 @@ import { awaitMessageOfType } from "../util";
 const APP_ID = "17608BC8";
 const AUTH_NS = "urn:x-cast:com.amazon.primevideo.cast";
 
+const DEFAULT_DOMAIN = "api.amazon.com";
+
 export interface IPrimeOpts {
     // TODO
     cookies: string;
     deviceId?: string;
     marketplaceId?: string;
-    token?: string;
+    refreshToken?: string;
+    domain?: string;
 }
 
 export class PrimeApp extends BaseApp {
@@ -39,7 +44,7 @@ export class PrimeApp extends BaseApp {
 
     public async play(titleId: string) {
         const session = await this.joinOrRunNamespace(AUTH_NS);
-        const resp = await request(session, this.message("AmIRegistered"));
+        const resp = await castRequest(session, this.message("AmIRegistered"));
         debug("registered=", resp);
 
         if (resp.error && resp.error.code === "NotRegistered") {
@@ -95,27 +100,66 @@ export class PrimeApp extends BaseApp {
     }
 
     private async register(session: ICastSession) {
-        debug("register with id", this.deviceId, "opts=", this.opts);
+        debug("register with id", this.deviceId);
+
+        const preAuthorizedLinkCode =
+            await this.generatePreAuthorizedLinkCode();
+
         await checkedRequest(session, this.message("Register", {
             // TODO load this from chakram
             marketplaceId: this.opts.marketplaceId,
 
-            preAuthorizedLinkCode: "",
+            preAuthorizedLinkCode,
         }));
 
-        await checkedRequest(session, this.message("AmIRegistered"));
+        // await checkedRequest(session, this.message("AmIRegistered"));
+    }
+
+    private async generatePreAuthorizedLinkCode() {
+        debug(`generating pre-authorized link code...`);
+        const response = await request.post({
+            body: {
+                auth_data: {
+                    access_token: this.opts.refreshToken,
+                },
+                code_data: {
+                    domain: "Device",
+
+                    app_name: "defaultAppName",
+                    app_version: "42",
+                    device_model: "Pixel",
+                    device_serial: this.deviceId,
+                    device_type: "android",
+                    os_version: "22",
+                },
+            },
+            json: true,
+            url: this.buildUrl("/auth/create/code"),
+        });
+
+        if (!response.code) {
+            throw new Error(JSON.stringify(response));
+        }
+
+        debug(`generated pre-authorized link code: ${response.code}`);
+        return response.code;
+    }
+
+    private buildUrl(path: string): string {
+        const domain = this.opts.domain || DEFAULT_DOMAIN;
+        return `https://${domain}/${path}`;
     }
 
 }
 
-async function request(session: ICastSession, message: any) {
+async function castRequest(session: ICastSession, message: any) {
     const responseType = message.type + "Response";
     session.send(message);
     return awaitMessageOfType(session, responseType);
 }
 
 async function checkedRequest(session: ICastSession, message: any) {
-    const resp = await request(session, message);
+    const resp = await castRequest(session, message);
     if (resp.error) {
         throw resp.error;
     }
