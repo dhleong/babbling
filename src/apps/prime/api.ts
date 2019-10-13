0d76987b7c264353e89e626e5be55fefa6b0cf7d
Daniel Leong <me@dhleong.net>
2019-10-13 16:45:20 -0400

    Refactor FRC cookie generation; strip references to the Java code

--- src/apps/prime/api.ts
+++ src/apps/prime/api.ts
@@ -24,6 +24,72 @@ const DEVICE_TYPE = "A43PXU4ZN2AL1";
 const OS_VERSION = "25";
 const SOFTWARE_VERSION = "2";
 
+// ======= utils ==========================================
+
+export async function generateFrcCookies(
+    deviceId: string,
+    language: string,
+) {
+    const cookies = JSON.stringify({
+        ApplicationName: APP_NAME,
+        ApplicationVersion: APP_VERSION,
+        DeviceLanguage: language,
+        DeviceName: "walleye/google/Pixel 2",
+        DeviceOSVersion: "google/walleye/walleye:8.1.0/OPM1.171019.021/4565141:user/release-keys",
+        IpAddress: getIpAddress(),
+        ScreenHeightPixels: "1920",
+        ScreenWidthPixels: "1280",
+        TimeZone: "-04:00",
+    });
+
+    debug("generating FRC cookies from: ", cookies);
+
+    // gzip
+    const zipped: Buffer = await new Promise((resolve, reject) => {
+        gzip(cookies, {}, (e, result) => {
+            if (e) reject(e);
+            else resolve(result);
+        });
+    });
+
+    // encrypt the cookies JSON
+    // don't ask about the salts used for the keys; I don't make the rules ;)
+    const key = createSaltedKey(deviceId, "AES/CBC/PKCS7Padding");
+    const iv = crypto.randomBytes(16);
+    const cipher = crypto.createCipheriv("aes-128-cbc", key, iv);
+    const cipheredBase = cipher.update(zipped);
+    const ciphered = Buffer.concat([cipheredBase, cipher.final()]);
+
+    // create an hmac digest containing the IV and the ciphered data
+    const hmac = crypto.createHmac(
+        "sha256",
+        createSaltedKey(deviceId, "HmacSHA256"),
+    );
+    hmac.update(iv);
+    hmac.update(ciphered);
+    const hmacd = hmac.digest();
+
+    // build the cookies buffer
+    const toBase64Encode = Buffer.concat([
+        Buffer.of(0),
+        hmacd.slice(0, 8),
+        iv,
+        ciphered,
+    ]);
+
+    return toBase64Encode.toString("base64");
+}
+
+function createSaltedKey(key: string, salt: crypto.BinaryLike) {
+    return crypto.pbkdf2Sync(
+        key,
+        salt,
+        1000, // iterations
+        16, // key length (in bytes, vs Java's bits)
+        "SHA1", // hash
+    );
+}
+
 // ======= public interface ===============================
 
 export class PrimeApi {
@@ -65,7 +131,7 @@ export class PrimeApi {
         registrationData.device_name = `${this.deviceNameBase}%DUPE_STRATEGY_2ND%`;
         body.registration_data = registrationData;
 
-        const frc = await this.generateFrcCookies();
+        const frc = await generateFrcCookies(this.deviceId, this.language);
         if (frc !== null) {
             debug(`generated cookies: ${frc}`);
             body.user_context_map = {
@@ -125,66 +191,6 @@ export class PrimeApi {
         return this.language;
     }
 
-    public async generateFrcCookies() {
-        const cookies = JSON.stringify({
-            ApplicationName: APP_NAME,
-            ApplicationVersion: APP_VERSION,
-            DeviceLanguage: this.language,
-            DeviceName: "walleye/google/Pixel 2",
-            DeviceOSVersion: "google/walleye/walleye:8.1.0/OPM1.171019.021/4565141:user/release-keys",
-            IpAddress: getIpAddress(),
-            ScreenHeightPixels: "1920",
-            ScreenWidthPixels: "1280",
-            TimeZone: "-04:00",
-        });
-
-        debug("generating FRC cookies from: ", cookies);
-
-        // gzip
-        const zipped: Buffer = await new Promise((resolve, reject) => {
-            gzip(cookies, {}, (e, result) => {
-                if (e) reject(e);
-                else resolve(result);
-            });
-        });
-
-        // Cipher instance = Cipher.getInstance("AES/CBC/PKCS5Padding");
-        // instance.init(1, b(str2, "AES/CBC/PKCS7Padding"));
-        const key = this.createSaltedKey("AES/CBC/PKCS7Padding");
-        const iv = crypto.randomBytes(16);
-        const cipher = crypto.createCipheriv("aes-128-cbc", key, iv);
-
-        // toByteArray = instance.doFinal(toByteArray);
-        // byte[] iv = instance.getIV();
-        const cipheredBase = cipher.update(zipped);
-        const ciphered = Buffer.concat([cipheredBase, cipher.final()]);
-
-        // Mac instance2 = Mac.getInstance("HmacSHA256");
-        // instance2.init(b(str2, "HmacSHA256"));
-        // instance2.update(iv);
-        // instance2.update(toByteArray);
-        // byte[] doFinal = instance2.doFinal();
-        const hmac = crypto.createHmac("sha256", this.createSaltedKey("HmacSHA256"));
-        hmac.update(iv);
-        hmac.update(ciphered);
-        const hmacd = hmac.digest();
-
-        // byte[] bArr = new byte[(toByteArray.length + 25)];
-        // bArr[0] = (byte) 0;
-        // System.arraycopy(doFinal, 0, bArr, 1, 8);
-        // System.arraycopy(iv, 0, bArr, 9, 16);
-        // System.arraycopy(toByteArray, 0, bArr, 25, toByteArray.length);
-        const toBase64Encode = Buffer.concat([
-            Buffer.of(0),
-            hmacd.slice(0, 8),
-            iv,
-            ciphered,
-        ]);
-
-        // return Base64.encodeToString(bArr, 2);
-        return toBase64Encode.toString("base64");
-    }
-
     private buildUrl(path: string): string {
         const domain = this.apiDomain();
         return `https://${domain}/${path}`;
@@ -192,25 +198,6 @@ export class PrimeApi {
 
     private apiDomain(): string {
         return this.opts.apiDomain || DEFAULT_API_DOMAIN;
-    }
-
-    private createSaltedKey(salt: crypto.BinaryLike) {
-        // return new SecretKeySpec(
-        //      SecretKeyFactory.getInstance("PBKDF2WithHmacSHA1")
-        //          .generateSecret(
-        //              new PBEKeySpec(
-        //                  str.toCharArray(), str2.getBytes("UTF-8"), 1000, 128
-        //              )
-        //          ).getEncoded(),
-        //          "AES"
-        //      );
-        return crypto.pbkdf2Sync(
-            this.deviceId,
-            salt,
-            1000, // iterations
-            16, // key length (in bytes, vs Java's bits)
-            "SHA1", // hash
-        );
     }
 
     private generateDeviceData(): any {
