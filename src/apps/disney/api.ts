4256e9033b77d196f26129ef7c19d0d4629edc53
Daniel Leong <me@dhleong.net>
2020-06-29 21:54:37 -0400

    Implement queryByTitle for DisneyApp

---
+++ src/apps/disney/api.ts
@@ -0,0 +1,110 @@
+import _debug from "debug";
+const debug = _debug("babbling:DisneyApp:api");
+
+import request from "request-promise-native";
+
+import { IDisneyOpts } from "./config";
+
+const GRAPHQL_URL_BASE = "https://search-api-disney.svcs.dssott.com/svc/search/v2/graphql/persisted/query/core/";
+const SEARCH_KEY = "disneysearch";
+const RESUME_SERIES_KEY = "ContinueWatchingSeries";
+
+export interface ISearchHit {
+    images: Array<{
+        purpose: string,
+        url: string,
+    }>;
+
+    mediaRights: {
+        downloadBlocked: true,
+        rewind: true,
+    };
+
+    milestones: Array<{
+        id: "95985aab-01a8-4ad0-948a-ed7c86b2a026",
+        milestoneTime: Array<{
+            startMillis: number,
+            type: "offset",
+        }>;
+        milestoneType: "up_next",
+    }>;
+
+    texts: Array<{
+        content: string,
+        field: "description" | "title",
+        language: string,
+        sourceEntity: "series" | "program",
+        targetEntity: "series" | "program",
+        type: "brief" | "full" | "medium" | "slug" | "sort",
+    }>;
+
+    contentId: string;
+    encodedSeriesId?: string;
+    episodeNumber?: number;
+    originalLanguage: string;
+    programType: "movie";
+    runtimeMillis: 5337000;
+    seasonId?: string;
+    seasonSequenceNumber?: number;
+    seriesId?: string;
+    videoId: string;
+}
+
+export class DisneyApi {
+
+    constructor(
+        private readonly options: IDisneyOpts,
+    ) {}
+
+    public async pickResumeEpisodeForSeries(seriesId: string) {
+        const data = await this.request(RESUME_SERIES_KEY, {
+            seriesId,
+        });
+
+        return data.resume as ISearchHit;
+    }
+
+    public async search(
+        query: string,
+    ) {
+        debug(`search: ${query}`);
+
+        const disneysearch = await this.request(SEARCH_KEY, {
+            index: "disney_global",
+            q: query,
+        });
+
+        return disneysearch.hits.map((obj: any) => obj.hit as ISearchHit) as ISearchHit[];
+    }
+
+    private async ensureToken() {
+        // TODO refresh if necessary?
+        return this.options.token;
+    }
+
+    private async request(
+        graphQlKey: string,
+        variablesMap: any,
+    ) {
+        const token = await this.ensureToken();
+
+        const variables = JSON.stringify({
+            preferredLanguage: ["en"],
+            ...variablesMap,
+        });
+
+        const { data } = await request({
+            headers: {
+                Authorization: `Bearer ${token}`,
+            },
+            json: true,
+            qs: {
+                variables,
+            },
+            url: GRAPHQL_URL_BASE + graphQlKey,
+        });
+
+        return data[graphQlKey];
+    }
+
+}
--- src/apps/disney/channel.ts
+++ src/apps/disney/channel.ts
@@ -1,37 +1,102 @@
+import _debug from "debug";
+const debug = _debug("babbling:DisneyApp:channel");
+
 import {
     // IEpisodeQuery,
     // IEpisodeQueryResult,
     IPlayableOptions,
     IPlayerChannel,
-    // IQueryResult,
+    IQueryResult,
 } from "../../app";
 
 // NOTE: this sure looks like a circular dependency, but we're just
 // importing it for the type definition
 import { DisneyApp, IDisneyOpts } from ".";
+import { DisneyApi } from "./api";
+
+const PLAYBACK_URL = "https://www.disneyplus.com/video/";
+const SERIES_URL = "https://www.disneyplus.com/series/";
 
 export class DisneyPlayerChannel implements IPlayerChannel<DisneyApp> {
 
+    private readonly api: DisneyApi;
+
     constructor(
         readonly options: IDisneyOpts,
-    ) {}
+    ) {
+        this.api = new DisneyApi(options);
+    }
 
     public ownsUrl(url: string): boolean {
         return url.includes("disneyplus.com");
     }
 
     public async createPlayable(url: string) {
-        // TODO other urls
+        // other urls?
         const videoMatch = url.match(/\/video\/(.+)$/);
-        if (!videoMatch || !videoMatch[1]) {
-            throw new Error(`Unsure how to play ${url}`);
+        if (videoMatch && videoMatch[1]) {
+            const id = videoMatch[1];
+
+            return async (app: DisneyApp, opts: IPlayableOptions) => {
+                await app.playById(id);
+            };
         }
 
-        const id = videoMatch[1];
+        const seriesMatch = url.match(/\/series\/[^\/]+\/(.+)$/);
+        if (seriesMatch && seriesMatch[1]) {
+            const seriesId = seriesMatch[1];
 
-        return async (app: DisneyApp, opts: IPlayableOptions) => {
-            await app.playById(id);
-        };
+            return async (app: DisneyApp, opts: IPlayableOptions) => {
+                // TODO: this probably belongs in the app:
+                debug("find resume for series", seriesId);
+
+                const resume = await this.api.pickResumeEpisodeForSeries(seriesId);
+                debug("... resume:", resume);
+                await app.playById(resume.contentId);
+            };
+        }
+
+        throw new Error(`Unsure how to play ${url}`);
+    }
+
+    /**
+     * Search for {@see Player.play}'able media by title
+     */
+    public async *queryByTitle(
+        title: string,
+    ): AsyncIterable<IQueryResult> {
+        const results = await this.api.search(title);
+
+        for (const result of results) {
+            const id = result.contentId;
+            const titleObj = result.texts.find(item => {
+                return item.field === "title" && item.type === "full";
+            });
+            const descObj = result.texts.find(item => {
+                return item.field === "description" && item.type === "full";
+            });
+
+            if (!titleObj) continue;
+
+            let url: string;
+            if (result.encodedSeriesId) {
+                const slugObj = result.texts.find(item => {
+                    return item.field === "title" && item.type === "slug";
+                });
+                url = SERIES_URL + slugObj!!.content + "/" + result.encodedSeriesId;
+            } else {
+                url = PLAYBACK_URL + id;
+            }
+
+            yield {
+                appName: "DisneyApp",
+                desc: descObj ? descObj.content : undefined,
+                title: titleObj.content,
+                url,
+
+                playable: await this.createPlayable(url),
+            };
+        }
     }
 
 }
