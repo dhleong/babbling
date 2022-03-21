dbd317e70689a8450cadecddd93dd59ddce7d109
Daniel Leong <me@dhleong.net>
2022-03-20 21:57:43 -0400

    Update the Configurable for Hbo

--- src/apps/hbo/config.ts
+++ src/apps/hbo/config.ts
@@ -13,7 +13,7 @@ export class HboConfigurable implements IConfigurable<IHboOpts> {
     public async extractConfig(
         source: IConfigSource,
     ) {
-        const stream = source.storage.readAll("https://play.hbogo.com");
+        const stream = source.storage.readAll("https://play.hbomax.com");
         for await (const { key, value } of stream) {
             if (key.includes("LoginInfo.user")) {
                 const entry = JSON.parse(value);
